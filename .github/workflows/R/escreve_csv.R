fc9683cf7c178a0c664372483f1ce792faa7e013
lpkist <137221454+lpkist@users.noreply.github.com>
2023-06-26 13:02:31 -0300

    Update escreve_csv.R

--- .github/workflows/R/escreve_csv.R
+++ .github/workflows/R/escreve_csv.R
@@ -1,6 +1,13 @@
 library(tidyverse)
 library(rugarch)
 library(yfR)
+get_day <- function() {
+                        data <- as.Date(Sys.time())
+                        dia_da_semana <- (as.numeric(format(data, "%u")) %% 7) + 1
+                        return(dia_da_semana)
+                      }
+dia_da_semana <- get_day()
+if(dia_da_semana %in% c(0,2){}else{
 get_data = function(tickers = "MSFT34.SA", first_date = "2013-01-01",
                     last_date = (Sys.Date())){
   data = yf_get(tickers, first_date, last_date)
@@ -10,23 +17,41 @@ get_data = function(tickers = "MSFT34.SA", first_date = "2013-01-01",
 }
 dados <- get_data()
 
-spec <- ugarchspec(mean.model = list(armaOrder = c(1,1),
+spec <- ugarchspec(mean.model = list(armaOrder = c(1,0),
                                     include.mean = FALSE),
-                  variance.model = list(model = 'sGARCH', garchOrder = c(1,1)),
+                  variance.model = list(model = 'sGARCH', garchOrder = c(1,2)),
                   distribution = "std")
 modelo <-  ugarchfit(spec, scale(dados$ret_adjusted, T, T), solver = 'hybrid')
 
-prev <- ugarchforecast(modelo, n.ahead = 5)
-prev <- cbind(prev@forecast$seriesFor, prev@forecast$sigmaFor)
+prev <- ugarchforecast(modelo, n.ahead = 1)
+alpha <- 0.01
+esp_t <- function(x, mu, sigma, shape) {
+  x*ddist(distribution = "std", 
+          y = x,
+          mu = mu, 
+          sigma = sigma, 
+          skew = 0,
+          shape = shape)
+}
+var <- qdist(distribution = "std", alpha,
+                  mu = prev@forecast[["seriesFor"]][1],
+                  sigma = sigma(prev)[1],
+                  skew = 0, shape = coef(modelo)["shape"])
+es <- integrate(esp_t, 
+                     -Inf, 
+                     var, 
+                     mu = prev@forecast[["seriesFor"]][1],
+                     sigma = sigma(prev)[1],
+                     shape = coef(modelo)["shape"])$value/alpha
+prev <- cbind(prev@forecast$seriesFor[1], var, es)
 rownames(prev) <- NULL
 prev <- as.data.frame(prev)
-sequencia <- seq(from = max(dados$date)+1, by = 1, length.out = 5)
+sequencia <- Sys.Date()
 prev$index <- sequencia
-colnames(prev) <- c("previsao","desvio_padrao","data")
-modelo <- ugarchfit(spec, dados$ret_adjusted, solver = 'hybrid')@fit
-print(prev)
+colnames(prev) <- c("Retorno", "VaR (1%)", "SE (1%)","Data")
 
 prevs_feitas <- read_csv('dados/previsao.csv')
 prevs <- rbind(prevs_feitas,prev)
 print(prevs)
 write_csv(prevs, "dados/previsao.csv")
+}
